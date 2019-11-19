1d125d17c97b64bd819bfc8f1f41faedb598f007
zakintaliban <nukmans23@gmail.com>
2019-11-19 14:49:35 +0700

    Added basic project layout

---
+++ src/components/navbar.js
@@ -0,0 +1,50 @@
+import React, { useState } from 'react'
+import { observer } from 'mobx-react'
+
+function Navbar(props) {
+  const [menu, setMenu] = useState(false)
+  const [value, setValue] = useState("")
+
+  const {addTodo} = props.store
+
+  const prepareAddTodo = (e) => {
+    e.preventDefault()
+    addTodo(value)
+    setValue("")
+  }
+
+  return (
+    <div className="navbar has-shadow">
+      <div className="navbar-brand">
+        <div className="navbar-item">
+          <p className="title">Easy Todo</p>
+        </div>
+
+        <a role="button" onClick={() => setMenu(!menu)} className={`navbar-burger burger ${menu ? "is-active" : ""}`} aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
+
+      <div className={`navbar-menu ${menu ? "is-active" : ""}`}>
+        <div className="navbar-end">
+          <div className="navbar-item">
+            <form onSubmit={prepareAddTodo} className="field has-addons">
+              <p className="control is-expanded">
+                <input value={value} type="text" onChange={(e) => setValue(e.target.value)} className="input" />
+              </p>
+              <p className="control">
+                <button className="button is-info has-text-weight-bold">
+                  Add Todo
+                </button>
+              </p>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default observer (Navbar)
---
+++ src/components/todoList.js
@@ -0,0 +1,38 @@
+import React from 'react'
+
+import Todo from "./todo";
+import { observer } from 'mobx-react';
+
+// could be done as well
+// @observer
+function TodoList(props) {
+  // Import the actions and properties
+  const { todos, toggleDone, deleteTodo, filteredTodos } = props.store
+
+  return (
+    <div>
+      <div className="hero is-info">
+        <div className="hero-body has-text-centered">
+          <p className="title is-1">{todos.length} Todos</p>
+        </div>
+      </div>
+
+      <section className="section">
+        <div className="container">
+          <input className="input" placeholder="Search..." type="text" value={props.store.filter} onChange={(e) => props.store.filter = e.target.value} />
+        </div>
+      </section>
+
+      <section className="section">
+        <div className="container">
+          {filteredTodos.map(todo => (
+            <Todo key={todo.id} todo={todo} toggleDone={toggleDone} deleteTodo={deleteTodo} />
+          ))}
+        </div>
+      </section>
+    </div>
+  )
+}
+
+// another way of using MobX
+export default observer(TodoList)
