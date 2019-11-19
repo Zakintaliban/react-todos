a6cd0da5ae4154087a3cdd29c7092438ba70c069
zakintaliban <nukmans23@gmail.com>
2019-11-19 15:59:35 +0700

    Updates

---
+++ precache-manifest.9ed7fd3612a463cd3e1094f942c362a7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a79f21909af03ded328579febc89476",
+    "url": "/react-todos/index.html"
+  },
+  {
+    "revision": "7b952f44df90d15a5b48",
+    "url": "/react-todos/static/css/main.59b7916e.chunk.css"
+  },
+  {
+    "revision": "7abff20ed5022cb3cc13",
+    "url": "/react-todos/static/js/2.d55dfb38.chunk.js"
+  },
+  {
+    "revision": "7b952f44df90d15a5b48",
+    "url": "/react-todos/static/js/main.68b38bbe.chunk.js"
+  },
+  {
+    "revision": "56c7bd169d694b33b0cd",
+    "url": "/react-todos/static/js/runtime-main.d4ac0a24.js"
+  }
+]);
